11a82d7173c9416fd24e217532dbeb5e23a9d3e0
Leonardo Gonçalves <leo13garbelotti@hotmail.com>
2024-02-23 11:39:29 -0300

    Add: Create Question and Fetch recent questions controllers

---
+++ src/controllers/create-question.controller.ts
@@ -0,0 +1,51 @@
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
+import { CurrentUser } from 'src/auth/current-user-decorator'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { UserPayload } from 'src/auth/jwt.stratedy'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe.pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class CreateQuestionsController {
+  constructor(private prisma: PrismaService) { }
+
+  @Post()
+  async handle(
+    @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload, // same as @Req() request: Request,
+  ) {
+    const { title, content } = body
+    const userId = user.sub
+
+    const slug = this.convertToSlug(title)
+
+    await this.prisma.question.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slug,
+      },
+    })
+  }
+
+  private convertToSlug(title: string): string {
+    return title
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/[^\w\s-]/g, '')
+      .replace(/\s+/g, '-')
+  }
+}
