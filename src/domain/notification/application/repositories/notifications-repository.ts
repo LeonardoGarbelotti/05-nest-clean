1c632c0faec442d56a3d93aff873bbe5d2e28f09
Leonardo Gonçalves <leo13garbelotti@hotmail.com>
2024-03-22 13:46:47 -0300

    Add: Notifications schema

--- src/domain/notification/application/repositories/notifications-repository.ts
+++ src/domain/notification/application/repositories/notifications-repository.ts
@@ -1,7 +1,7 @@
 import { Notification } from '../../enterprise/entities/notification'
 
-export interface NotificationsRepository {
-  findById(id: string): Promise<Notification | null>
-  update(notification: Notification): Promise<void>
-  create(notification: Notification): Promise<void>
+export abstract class NotificationsRepository {
+  abstract findById(id: string): Promise<Notification | null>
+  abstract update(notification: Notification): Promise<void>
+  abstract create(notification: Notification): Promise<void>
 }
