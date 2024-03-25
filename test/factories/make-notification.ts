d41a39aaae5203261fab651571064e2e12958717
Leonardo Gonçalves <leo13garbelotti@hotmail.com>
2024-03-25 11:14:13 -0300

    Add: Read notification

--- test/factories/make-notification.ts
+++ test/factories/make-notification.ts
@@ -3,8 +3,11 @@ import {
   Notification,
   NotificationProps,
 } from '@/domain/notification/enterprise/entities/notification'
+import { PrismaNotificationMapper } from '@/infra/database/prisma/mappers/prisma-notification-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 
 import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 
 export function MakeNotification(
   override: Partial<NotificationProps> = {},
@@ -21,4 +24,21 @@ export function MakeNotification(
   )
 
   return notification
+}
+
+@Injectable()
+export class NotificationFactory {
+  constructor(private prisma: PrismaService) { }
+
+  async makePrismaNotification(
+    data: Partial<NotificationProps> = {},
+  ): Promise<Notification> {
+    const notification = MakeNotification(data)
+
+    await this.prisma.notification.create({
+      data: PrismaNotificationMapper.toPrisma(notification),
+    })
+
+    return notification
+  }
 }
--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -3,6 +3,7 @@ import { config } from 'dotenv'
 import { PrismaClient } from '@prisma/client'
 import { randomUUID } from 'node:crypto'
 import { execSync } from 'node:child_process'
+import { DomainEvents } from '@/core/events/domain-events'
 
 config({ path: '.env', override: true })
 config({ path: '.env.test', override: true })
@@ -28,7 +29,7 @@ beforeAll(async () => {
 
   process.env.DATABASE_URL = databaseURL
 
-  console.log(databaseURL)
+  DomainEvents.shouldRun = false
 
   execSync('npx prisma migrate deploy')
 })
